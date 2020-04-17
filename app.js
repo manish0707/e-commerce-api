a5ff3b5b34c36599ff93b5f72dffc076498de7fe
Manish Sharma <manishsharma959939@gmail.com>
2020-04-17 21:16:44 +0530

    feat: wrote code for DB connection

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology : true,
+  useCreateIndex: true
+})
+.then(() => {
+  console.log(`DB IS CONNECTED!`)
+})
+.catch((e) => {
+  console.log("NOT CONNECTED!")
+})
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`App is running at ${port}`);
+});
