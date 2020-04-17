3cf1ed3fbcac3ce076c36e829e703af4e9fe664e
Manish Sharma <manishsharma959939@gmail.com>
2020-04-17 21:24:29 +0530

    feat: adds inbuild middleware like bodyparser/cookieparser/cors

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
 const app = express();
+
+// Database connection code
 
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
@@ -16,6 +21,12 @@ mongoose.connect(process.env.DATABASE, {
 })
 
 const port = process.env.PORT || 8000;
+
+// Using middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors())
+
 
 app.listen(port, () => {
   console.log(`App is running at ${port}`);
