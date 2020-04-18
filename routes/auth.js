b5ffbe1ec9975f2e10b22b92415f7abf91f33d5d
Manish Sharma <manishsharma959939@gmail.com>
2020-04-18 12:00:45 +0530

    feat: adds signup route validation using express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { signout, signup } = require("../controllers/auth")
+const { check } = require("express-validator");
 
+const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup", signup)
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("Name should be atleast three character!"),
+    check("email").isEmail().withMessage("Email is not valid!"),
+    check("password")
+      .isLength({ min: 3 })
+      .withMessage("Password is too small...!!"),
+  ],
+  signup
+);
+router.get("/signout", signout);
 
-
-router.get("/signout", signout)
-
-module.exports = router
+module.exports = router;
